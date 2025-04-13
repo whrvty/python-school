c280c85a324a8d9b204d5add29b7d0e4f34ae930
whrvty <cwj121977@gmail.com>
2025-04-13 09:37:09 +0900

    36
    36

---
+++ 36_숫자출력_1116조원진.py
@@ -0,0 +1,24 @@
+# for i in range(5):
+#     print("1")
+
+# for i in range(1,6):
+#     print(f"{i}번째 1")
+
+# for i in range(1,101):
+#     print(i)
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+# n = int(input("어디까지 출력할까요?:"))
+# for i in range(1,n+1):
+#     print(i)
+
+# n = int(input("어디가지 출력할까요?:"))
+# for i in range(n, -1, -1):
+#     print(i)
+    
+# n = int(input("몇 단:"))
+# for i in range(1,10):
+#     print(f"{n}*{i}={i*n}")
+
