581375fa05223597bd2cd03a5a104b6bf5d97d49
whrvty <cwj121977@gmail.com>
2025-04-26 09:41:55 +0900

    46
    46

---
+++ 46_문자열계산_1116조원진.py
@@ -0,0 +1,14 @@
+# sen = "Hello. I am studying python."
+# print("#"*20)
+# print(sen.upper())
+# print(sen.lower())
+# print(len(sen))
+# print(sen.split())
+# print(sen.split("."))
+# print(sen.find("o"))
+# print((sen.count("o")))
+# print(sen.replace("python", "AI"))
+
+array = [0, 1, 2, 3]
+result = ["even" if num % 2 == 0 else "odd" for num in array]
+print(result)
