ebd63cb781fb2ba29981ff91de5603dfd2b76895
whrvty <cwj121977@gmail.com>
2025-04-27 09:42:29 +0900

    51
    51

---
+++ 51_함수_기본_1116조원진.py
@@ -0,0 +1,23 @@
+# def fun_s(n):
+#     s = 0
+#     for i in range(1, n+1):
+#         s += i
+#     return s
+# print(fun_s(10))
+
+# def fun_fact(n):
+#     s = 1
+#     for i in range(1, n+1):
+#         s *= i
+#     return s
+# print(fun_fact(10))
+
+a,b = map(int, input(). split())
+def maxp(n,m):
+    if n > m:
+        return n 
+    else:
+        return m
+print(maxp(a,b))
+
+    
