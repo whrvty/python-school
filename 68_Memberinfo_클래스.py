49b5fd8134e871ca3c86e3808d1ee2fa9591927f
whrvty <cwj121977@gmail.com>
2025-05-01 09:43:51 +0900

    68
    68

---
+++ 68_Memberinfo_클래스.py
@@ -0,0 +1,10 @@
+class Memberinfo:
+    def __init__(self,idx,passwd,name):
+        self.idx = idx
+        self.passwd = passwd
+        self.name = name
+    def getMember(self):
+        return f"{self.idx}, {self.passwd}, {self.name}"
+    
+my_member = Memberinfo("king", "123456", "홍길동") 
+print(my_member.getMember())
